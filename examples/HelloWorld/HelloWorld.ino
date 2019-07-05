afdc730d96b1fa93d4b2f23aa9ee3cb4a926c54b
Hyeonki Hong <hhk7734@gmail.com>
2019-07-05 17:45:54 +0900

    Add example

---
+++ examples/HelloWorld/HelloWorld.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd( 0x27, 16, 2 );
+
+void setup()
+{
+    // initialize the LCD
+    // lcd.begin( &Wire3);
+    lcd.begin();
+
+    lcd.print( "Hello, world!" );
+    lcd.setCursor( 0, 1 );
+    lcd.print( "LiquidCrystal" );
+}
+
+void loop()
+{
+    // Do nothing here...
+}
